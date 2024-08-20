d469cb94f04e9e6cbb50d3535c25b160d0a01043
S2009 <S2009.developer1@laposte.net>
2024-08-20 12:23:00 +0200

    Update to version 1.0.1

--- API/index.php
+++ API/index.php
@@ -36,6 +36,24 @@ if (isset($_GET['setup'])){
         header("Location: https://solarus-games.org/");
         exit;
     }
+} elseif (isset($_GET["cert"])) {
+    $file = 'Solarus.cer';
+
+    if (file_exists($file)) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="' . basename($file) . '"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($file));
+
+        readfile($file);
+        exit;
+    } else {
+        header("Location: https://solarus-games.org/");
+        exit;
+    }
 } else {
     header("Location: https://solarus-games.org/");
     exit;
